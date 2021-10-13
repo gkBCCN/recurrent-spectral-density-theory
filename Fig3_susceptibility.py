965136e484d521167fca776f7604aaad51d9f09b
Mr User <mr@user.com>
2021-10-13 14:54:02 +0200

    ADD: susc. Fig 3.

---
+++ Fig3_susceptibility.py
@@ -0,0 +1,48 @@
+import matplotlib.pyplot as plt
+import joblib
+from param_file import *
+import allspike_theory
+from allspike_theory import delay_characteristic_func
+from allspike_theory import input_mean_and_intensity
+from matplotlib import rc
+rc('font', **{'size': 22, })
+
+rm_col = '#DF1EA8E3'
+sim_col = 'k'
+pop_th_col = '#E0E0E0'
+
+
+theory_freq = stat_params['theory_freq']
+plt.figure(figsize=(5, 3.5))
+ks_results = joblib.load(dp + f'KS_w_signal_ave_over_1000_trials')
+X_sim = ks_results['Sas'] / ks_results['Ss']
+X_sim_mag = np.sqrt(X_sim.real ** 2 + X_sim.imag ** 2)
+plt.plot(ks_results['freqs'][1:], X_sim_mag[1:], lw=2, c=sim_col, label='sim', zorder=1, alpha=.8)
+
+r0 = joblib.load(dp + f'ISI_ave_over_1000_trials')['r0']
+
+X_curr, X_noise, X_rm = allspike_theory.load_susceptibilities(sim_params, stat_params, dp)
+plt.plot(theory_freq, X_rm['mag'], c=rm_col, lw=3, ls='-', label=r'$\chi_{RM}$-with $\chi_D$', alpha=1, zorder=5)
+
+if J==0.01:
+    pd = delay_characteristic_func(sim_params, stat_params['theory_freq'])
+    R_curr, R_noise = input_mean_and_intensity(sim_params)
+    X_no_D = X_curr['complex'] / (1 - X_curr['complex'] * R_curr * pd)
+    X_no_D_mag = np.sqrt(X_no_D.real**2 + X_no_D.imag**2)  # this is more accurate than np.abs()
+    plt.plot(theory_freq, X_no_D_mag, c='C9', lw=3, ls='-', label=r'$\chi_{RM}$-no $\chi_D$', alpha=1, zorder=5)
+
+plt.ylabel(r'$|\chi|$', rotation=0, labelpad=20)
+plt.xscale('log')
+xtick_arr = [0.01, 0.1, 1, r0, 10, fc]
+xtick_str = ['$10^{-2}$', '$10^{-1}$', '$10^{0}$', r'$r_0$', '$10^{1}$', '']
+plt.axvline(r0, c='k', ls=':', lw=1, zorder=0)
+plt.xticks(xtick_arr, xtick_str)
+plt.xlabel('f')
+plt.xlim(0.05, fc)
+plt.gca().spines['right'].set_visible(0)
+plt.gca().spines['top'].set_visible(0)
+plt.ylim(0, 3)
+plt.yticks([1, 3])
+plt.subplots_adjust(top=0.95, right=0.97, left=0.16, bottom=0.2)
+plt.legend(fontsize=16, frameon=0)
+plt.show()
--- param_file.py
+++ param_file.py
@@ -7,9 +7,9 @@ dp = 'data/'
 #                       Model Parameters                                   #
 # ------------------------------------------------------------------------ #
 sim_params = dict()
-sim_params['T'] = 1000                      # trial length (recording time)
-sim_params['dt'] = 1e-3                     # time step
-sim_params['burn_in'] = 200                 # period before measurement to negate initial cond. artifacts
+sim_params['T'] = T = 1000                  # trial length (recording time)
+sim_params['dt'] = dt = 1e-3                # time step
+sim_params['burn_in'] = burn_in = 200       # period before measurement to negate initial cond. artifacts
 
 net_params = dict()
 net_params['N_e'] = N_e = int(10e3)         # excitatory population size
@@ -20,25 +20,25 @@ sim_params['net'] = net_params
 neuron_params = dict()
 neuron_params['vt'] = vt = +1.              # threshold
 neuron_params['vr'] = vr = 0.               # reset
-neuron_params['vl'] = 0.                    # leak
-neuron_params['taum'] = 1.                  # membrane time constant
-neuron_params['tref'] = 0.1                 # refractory period
+neuron_params['vl'] = vl = 0.               # leak
+neuron_params['taum'] = taum = 1.           # membrane time constant
+neuron_params['tref'] = tref = 0.1          # refractory period
 sim_params['neuron'] = neuron_params
 
 signal_params = dict()
 signal_params['f_c'] = fc = 15.             # signal cutoff frequency
-signal_params['sig_s'] = 0.3                # external signal standard deviation
-signal_params['D_s'] = 1 / (4 * fc)         # external noise intensity -> gives var=1
-signal_params['mu'] = 1.1 * (vt - vr)       # bias
-signal_params['D'] = 1.0e-2                 # intrinsic noise intensity
+signal_params['sig_s'] = sig_s = 0.3        # external signal standard deviation
+signal_params['D_s'] = Ds = 1 / (4 * fc)    # external noise intensity -> gives var=1
+signal_params['mu'] = mu = 1.1 * (vt - vr)  # bias
+signal_params['D'] = D = 1.0e-2             # intrinsic noise intensity
 sim_params['signal'] = signal_params
 
 synapse_params = dict()
-synapse_params['J'] = J = 1.0e-2                # (excitatory) synaptic strength
-synapse_params['g'] = 5.                    # relative inhibitory strength (4=balanced if gamma=0.25)
-synapse_params['Ci'] = 200                  # fixed number of inhibitory inputs
+synapse_params['J'] = J = 1.0e-2            # (excitatory) synaptic strength
+synapse_params['g'] = g = 5.                # relative inhibitory strength (4=balanced if gamma=0.25)
+synapse_params['Ci'] = Ci = 200             # fixed number of inhibitory inputs
 synapse_params['Ce'] = Ce = 800             # fixed number of excitatory inputs
-synapse_params['p_conn'] = Ce / N_e         # connection probability (sparseness)
+synapse_params['p_conn'] = pc = Ce / N_e    # connection probability (sparseness)
 synapse_params['delay'] = [0.05, 0.2]       # synaptic delay
 sim_params['synapse'] = synapse_params
 
